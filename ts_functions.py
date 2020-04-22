dedf4a96918cb194bd011de6b3ac8fdf922acccc
rafia37 <rafia0037@gmail.com>
2020-04-22 14:16:26 -0500

    Initializing and basic cleanup

---
+++ ts_functions.py
@@ -0,0 +1,24 @@
+#Functions used by Tabu Search
+
+import numpy as np
+
+
+def tabu_criteria():
+    return
+
+def tabu_tenure():
+    return
+
+def aspiration_criteria():
+    return
+
+#Short term memory
+def st_memory():
+    return
+
+#Long term memory
+def lt_memory():
+    return
+
+def path_relinking():
+    return
