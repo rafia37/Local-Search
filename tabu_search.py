f7839f09ef7c03920e5bcdca157d6105afb2bc84
rafia37 <rafia0037@gmail.com>
2020-04-23 11:02:20 -0500

    basic logic in place. Untested

--- tabu_search.py
+++ tabu_search.py
@@ -36,12 +36,12 @@ x_curr = cf.initial_solution()
 f_curr = cf.evaluate(x_curr)
 
 #memory initialization
-st_mem = ts.st_memory(init=True)
+sMem, sMemVal = ts.st_memory(init=True)
 
 #initial neighborhood
 N = cf.neighborhood(x_curr)
 
-
+pdb.set_trace()
 
 #----------------------
 #Tabu Search Algorithm
@@ -53,38 +53,66 @@ done = 0
 #A counter to count number of iterations
 counter = 0
 
-while done==0:
+#keeping track of all the solutions
+solutions = []
+sol_values = []
+sol_weights = []
+
+while done==0:    
+    solutionsChecked += 1
+
+    #indices of tabu-unactive members
+    tu_ind = np.where(sMem==0)
 
     #tabu-unactive subset of neighborhood N
-    tu_ind = ts.tabu_active()       #indices of tabu-unactive members
-    N_star = N[]
+    N_star = N[tu_ind]
 
-    #Selecting a candidate solution
-    #-----some extended cost func-------
-    s = N_star[]
-    f_s = cf.evaluate(s)
+    #Selecting a candidate
+    
+    #if all the solutions are tabu:
+    if len(N_star)==0:
+        all_val = [cf.evaluate(s)[0] for s in N]
+        ts.aspiration_criteria(neighborhood=N, values=all_val)
+    else:
+        #otherwise -
+        #Pick the solution with the best value 
+        #from non-tabu members even if they are non-improving 
+        s_values = [cf.evaluate(s)[0] for s in N_star]
+        s = N_star[np.nanargmax(s_values)]
+        f_s = cf.evaluate(s)
 
-    #updating
+    #Finding where the flip occurred
+    tabu_ind = ts.tabu_criteria(s, x_curr)
+    
+    #updating all variables
+    sMem, sMemVal = ts.st_memory(update_ind = tabu_ind, 
+                                 mem = sMem, 
+                                 solution = s)
+
     x_curr = np.copy(s)
     f_curr = np.copy(f_s)
 
-    st_mem = ts.st_memory()
+    solutions.append(x_curr)
+    sol_values.append(f_curr[0])
+    sol_weights.append(f_curr[1])
 
     N = cf.neighborhood(x_curr)
 
     #stopping criteria
     counter += 1
-    print("----- while loop iteration : {} ----- \n".format(counter))
-    logger.info("----- while loop iteration : {} ----- \n".format(counter))
+    print("iteration: {}, current_solution: {} \n".format(counter, f_curr[0]))
+    logger.info("iteration: {}, current_solution: {} \n".format(counter, f_curr[0]))
 
     if counter == 5:
         done
 
 
-
+best_val = np.nanmax(sol_values)
+best_weight = sol_weights[np.nanargmax(sol_values)]
+best_solution = solutions[np.nanargmax(sol_values)]
     
-print ("\nFinal number of solutions checked: ", solutionsChecked)
-print ("Best value found: ", f_best[0])
-print ("Weight is: ", f_best[1])
-print ("Total number of items selected: ", np.sum(x_best))
-print ("Best solution: ", x_best)
+print ("\n Final number of solutions checked: ", solutionsChecked)
+print ("Best value found: ", best_val)
+print ("Weight is: ", best_weight)
+print ("Total number of items selected: ", np.sum(best_solution))
+print ("Best solution: ", best_solution)
